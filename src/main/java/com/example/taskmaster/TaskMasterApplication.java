1887f87a72bc663f3bc056a4d181ce6d5bd07624
Venkata Siddardha Rali <sidhurv8@gmail.com>
2025-01-18 17:53:53 +0530

    added monitoring with prom

--- src/main/java/com/example/taskmaster/TaskMasterApplication.java
+++ src/main/java/com/example/taskmaster/TaskMasterApplication.java
@@ -2,8 +2,9 @@ package com.example.taskmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
+@EnableWebMvc
 public class TaskMasterApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaskMasterApplication.class, args);
